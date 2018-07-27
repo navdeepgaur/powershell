7d8a8dabbc2c3b42558efeb563012a1fc8a1caa8
navdeepgaur <sendyourmailformehere@gmail.com>
2018-07-27 19:17:16 +0530

    Removed useless try catch

--- ESCIEDisable/ESCIEDisable.ps1
+++ ESCIEDisable/ESCIEDisable.ps1
@@ -26,13 +26,7 @@ function Disable-IEESC
 {
 $AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
 $UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
-    try{
-        
-    }catch{
-          
-          }
-
-    try {
+   try {
         Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
         Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
         Stop-Process -Name Explorer
@@ -42,8 +36,6 @@ $UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1
         Write-Host “Failed to disable IE Enhanced Security Configuration (ESC).” -ForegroundColor Red
         exit
         }
-        
-    
 }
 
 
